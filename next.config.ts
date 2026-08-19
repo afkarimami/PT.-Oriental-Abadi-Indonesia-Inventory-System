6b997f574e181d87af152954043aec2cbf45fa22
afkarimami <afkarimami@student.ppns.ac.id>
2026-08-19 13:49:57 +0700

    Made it simple from next.config.ts for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const nextConfig: NextConfig = {
-  distDir: process.env.VERCEL ? ".next" : ".next-build-user-delete",
-  turbopack: {
-    root: __dirname,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
