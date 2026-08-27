b8114af5f3d084996be9cfa1d538af5868fa7f5b
afkarimami <afkarimami@student.ppns.ac.id>
2026-08-27 14:08:03 +0700

    fix: env.local

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -7,7 +7,7 @@ import { NextResponse, type NextRequest } from "next/server";
 export async function updateSession(request: NextRequest) {
 
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  // Ubah ke NEXT_PUBLIC_SUPABASE_ANON_KEY agar sesuai dengan .env.local
+  
   const publishableKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   let response = NextResponse.next({ request });
